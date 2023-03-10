cf26d8d34c3f9047ed2828e5ee7cddc28c15c51b
Kartik Mishra <kartikmishra.km44@gmail.com>
2023-03-10 23:04:23 +0530

    Twitter smart contract added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    ploygon: {
+      url: process.env.ALCHEMY_CONNECTION_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KAY]
+    }
+  }
 };
